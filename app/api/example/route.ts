f4e88699c56d4e55836dfe12e70735986ed41d7d
kazuhito_m <kazuhito.sumpic@gmail.com>
2025-04-02 16:14:13 +0900

    feat: APIでSlackに送信できるように。

--- app/api/example/route.ts
+++ app/api/example/route.ts
@@ -1,4 +1,20 @@
-export function GET(request: Request) {
-    console.log("これが呼ばれた時には、サーバ側に何か刻むように。");
-    return new Response("え、API作るだけで、しかも日本語でええのん？");
-}
+export const GET = async (request: Request) => {
+    let resultMessage = "環境変数が見つからなかったので、何もしませんでした。";
+    const slackWebhookUrl = process.env["SLACK_WEBHOOK_URL"];
+    if (slackWebhookUrl != null) {
+        await fetch(slackWebhookUrl, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                deploymentId: "テスト用、正規のIDではありません",
+                parameterSheetUrl: "未設定",
+                message: "ここに好きなメッセージを貼ります。"
+            }),
+        });
+        resultMessage = "テスト用のメッセージはSlackに送信されました。"
+    }
+    console.log(resultMessage);
+    return new Response(resultMessage);
+};
